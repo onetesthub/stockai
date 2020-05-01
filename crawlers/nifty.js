898888ae4400df38c73ffce4683b6eb490f912ef
rdhaliwal97 <rdhaliwal97@outlook.com>
2020-05-02 01:43:38 +0530

    values mapped to keys

--- crawlers/nifty.js
+++ crawlers/nifty.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const publiser = require('../publisher/publisher');
+const publiser = require("../publisher/publisher");
 const queue = new publiser();
 const niftyBaseUrl = "https://www1.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp";
 const stockBaseUrl = null;
@@ -15,14 +15,13 @@ const getCurrStrikeData = (tableRows, currentIndex, $) => {
       return false; //same as break for loops
     }
   });
-  return { currStrikePriceIndex: currIndex, currentValueRoundOff: currValue } ;
-}
+  return { currStrikePriceIndex: currIndex, currentValueRoundOff: currValue };
+};
 
 const getWeeks = async (weekCount = 3) => {
-
   const response = await axios.get("https://www1.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp");
   $ = cheerio.load(response.data);
-  
+
   let dateDropDown = $("#date option");
   let dateArray = [];
   dateDropDown.each((index, element) => {
@@ -34,28 +33,33 @@ const getWeeks = async (weekCount = 3) => {
 };
 
 const getCurrentNiftyValue = async ($) => {
-  let currentValuelabel = $("#wrapper_btm table tbody tr div").last().children('span').first().children('b').first().html().trim();
+  let currentValuelabel = $("#wrapper_btm table tbody tr div").last().children("span").first().children("b").first().html().trim();
   return {
-    currentIndex: currentValuelabel.split(' ')[0],
-    currentValue: Number(currentValuelabel.split(' ')[1])
-  }
+    currentIndex: currentValuelabel.split(" ")[0],
+    currentValue: Number(currentValuelabel.split(" ")[1]),
+  };
 };
 
-const getCrawlUrl = (symbol, week) =>{
-  if(symbol.toLowerCase() == "nifty" || symbol.toLowerCase() == "banknifty") return `https://www1.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?segmentLink=17&instrument=OPTIDX&symbol=${symbol}&date=${week}`
-  else return `HDFC URL`;
-};
+const getCrawlUrl = (symbol, week) => `https://www1.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?segmentLink=17&instrument=OPTIDX&symbol=${symbol}&date=${week}`;
 
 const getNiftyData = async (crawlUrl, symbol, week) => {
   const respoonse = await axios.get(crawlUrl);
 
   const $ = cheerio.load(respoonse.data);
+  
+  let headers = [];
+  let tableHeaders = $("#octable thead tr");
+  let headersRow = $(tableHeaders[1]);
+  headersRow.children("th").each((i, e) => {
+    headers.push($(e).text().trim());
+  });
+  
   let tableRows = $("#octable tbody tr");
 
   const { currentValue } = await getCurrentNiftyValue($);
 
   const { currStrikePriceIndex, currentValueRoundOff } = await getCurrStrikeData(tableRows, currentValue, $);
-  
+
   if (tableRows.length > currStrikePriceIndex + 20)
     tableRows.splice(currStrikePriceIndex + 20, tableRows.length); // removing enteries from end
 
@@ -63,51 +67,46 @@ const getNiftyData = async (crawlUrl, symbol, week) => {
   if (currStrikePriceIndex > 20 && tableRows.length > 20)
     tableRows.splice(0, currStrikePriceIndex - 20); // removing enteries from starting
   else temp = currStrikePriceIndex;
-  
-  const symbolData = [];
+
   tableRows.each((index, element) => {
     const strikePrice = Number( $(element).children(".grybg").children("a").last().children("b").last().html().trim() );
-    
-    let set1 = [], set2 = [];
-    
-    $(element).children('td').each((index, elem)=>{
-      if($(elem).hasClass('nobg')) set1.push(Number(($(elem).text().trim() == '-')? 0 : ($(elem).text().trim()).replace(',', '')));
-      else if($(elem).hasClass('ylwbg')) set2.push(Number(($(elem).text().trim() == '-')? 0 : ($(elem).text().trim()).replace(',', '')));
-    });
 
     let callObj = { type: "call", symbol, expiryDate: week, currentValue, strikePrice },
       putObj = { type: "put", symbol, expiryDate: week, currentValue, strikePrice };
 
-    if(strikePrice < currentValueRoundOff){
-      callObj.values = set2;
-      putObj.values = set1;
-    }else{
-     callObj.values = set1;
-     putObj.values = set2;
-    }
+    $(element).children('td').each((index, elem)=>{
+      let currValue = Number(($(elem).text().trim() == '-')? 0 : ($(elem).text().trim()).replace(',', ''));
+      if(strikePrice < currentValueRoundOff){
+        if($(elem).hasClass('nobg')){
+          putObj[headers[index]] = currValue;
+        } else if ($(elem).hasClass("ylwbg")) {
+          callObj[headers[index]] = currValue;
+        }
+      } else {
+        if ($(elem).hasClass("nobg")) {
+          callObj[headers[index]] = currValue;
+        } else if ($(elem).hasClass("ylwbg")) {
+          putObj[headers[index]] = currValue;
+        }
+      }
+    });
 
-    if(index < temp - 5) queue.push(putObj);
-    else if(index > temp + 4) queue.push(callObj);
-    else{
+    if (index < temp - 5) queue.push(putObj);
+    else if (index > temp + 4) queue.push(callObj);
+    else {
       queue.push(callObj);
       queue.push(putObj);
     }
-
   });
-  return true
-}
+  return true;
+};
 
-const getStockData = async (crawlUrl, symbol, week) =>{
-  return null
-}
+const getStockData = async (crawlUrl, symbol, week) => {
+  return null;
+};
 
-const getSymbolData = async (symbol, week)=>{
+const getSymbolData = async (symbol, week) => {
   const crawlUrl = getCrawlUrl(symbol, week);
-  let data;
-  if(symbol.toLowerCase() == "nifty" || symbol.toLowerCase() == "banknifty") {
-    data = await getNiftyData(crawlUrl, symbol, week);
-  }else{
-    // data = await getStockData(crawlUrl, symbol, week);
-  }
-}
-module.exports = { getSymbolData, getWeeks }
+  let data = await getNiftyData(crawlUrl, symbol, week);
+};
+module.exports = { getSymbolData, getWeeks };
--- scheduler/nifty.js
+++ scheduler/nifty.js
@@ -1,6 +1,6 @@
 const { getSymbolData, getWeeks } = require('./../crawlers/nifty');
 
-const symbols = ["NIFTY", "BANKNIFTY", "HDFC"];
+const symbols = ["NIFTY", "BANKNIFTY"];
 // const symbols = ["NIFTY"]; //* for testing
 
 const crawler = async (symbol, week) => {
@@ -9,13 +9,14 @@ const crawler = async (symbol, week) => {
 
 async function runBot(symbols) {
   const weeks = await getWeeks();
-  
-  // setInterval(() => {
+  // const weeks = ['30APR2020']; //* for testing
+
+  setInterval(() => {
     for (const symbol of symbols) {
       for (const week of weeks) {
         crawler(symbol, week);
       }
     }
-  // }, 10000 || 300000);
+  }, 10000 || 300000);
 } 
 runBot(symbols);
