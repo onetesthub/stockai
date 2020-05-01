f75e05bc606401d5fb6350b39934f0975ce241b8
zero2zero <sandeep@onetesthub.com>
2020-04-30 20:13:59 -0700

    added us stock crawler

---
+++ scheduler/nifty.js
@@ -0,0 +1,22 @@
+const { getSymbolData, getWeeks } = require('./crawler.js');
+
+const symbols = ["NIFTY", "BANKNIFTY", "HDFC"];
+// const symbols = ["NIFTY"]; //* for testing
+
+const crawler = async (symbol, week) => {
+  getSymbolData(symbol, week);
+};
+
+async function runBot(symbols) {
+  const weeks = await getWeeks(); 
+  console.log('weeks :>> ', weeks);
+  
+  setInterval(() => {
+    for (const symbol of symbols) {
+      for (const week of weeks) {
+        crawler(symbol, week);
+      }
+    }
+  }, 10000 || 300000);
+} 
+runBot(symbols);
